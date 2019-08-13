d13a2b19bf29470f207560752021693fa242b5c1
Sergio Garcia Prado <sergio@garciparedes.me>
2019-08-13 15:55:43 +0200

    * Implemented caching on "Position.distance(...)" method.

--- jinete/models/positions.py
+++ jinete/models/positions.py
@@ -30,14 +30,20 @@ logger = logging.getLogger(__name__)
 class Position(ABC):
     uuid: UUID
 
-    def __init__(self, surface: Surface = None, uuid: UUID = None):
+    def __init__(self, surface: Surface = None, uuid: UUID = None, with_caching: bool = True):
         if uuid is None:
             uuid = uuid4()
+
         self.surface = surface
         self.uuid = uuid
 
+        self.with_caching = with_caching
+        self._distance = None
+
     def distance_to(self, other: Position) -> float:
-        return self.surface.distance(self, other)
+        if self._distance is None or not self.with_caching:
+            self._distance = self.surface.distance(self, other)
+        return self._distance
 
     def time_to(self, other: Position, now: float) -> float:
         return self.surface.time(self, other, now)
