ab97dd8fbf7f6011b24e9ed97485f50b3f2dc8a2
Sergio Garcia Prado <sergio@garciparedes.me>
2019-08-13 19:06:47 +0200

    * Fixed #17 bug (related with changes related with the implementation of "Objective" object.

--- jinete/algorithms/utils/crossers/randomized.py
+++ jinete/algorithms/utils/crossers/randomized.py
@@ -34,8 +34,11 @@ class RandomizedCrosser(OrderedCrosser):
             if len(sub_ranking) == 0:
                 continue
             for current in sub_ranking.values():
-                if not (len(candidates) == 0 or candidates[-1] < current):
-                    break
+                if len(candidates) != 0:
+                    best = self.objective.best_planned_trip(candidates[-1], current)
+                    if not best != candidates[-1]:
+                        break
+
                 if self.randomized_size < len(candidates):
                     candidates.pop()
                 candidates.append(current)
