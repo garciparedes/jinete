1c78b15bff3d54f2953bc728bfae0cb23e72a5cc
Sergio Garcia Prado <sergio@garciparedes.me>
2019-08-15 18:38:41 +0200

    * Fixed bug related with sphinx documentation on readthedocs. #10

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,15 +23,12 @@ import jinete as jit
 project = jit.__name__
 copyright = '2019, Sergio García Prado'
 author = 'Sergio García Prado'
-
-# The full version, including alpha/beta/rc tags
 release = jit.__version__
 
 # -- General configuration ---------------------------------------------------
 
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
+master_doc = 'index'
+
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.coverage',
@@ -53,21 +50,11 @@ exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
 
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = 'sphinx_rtd_theme'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
 # -- Extension configuration -------------------------------------------------
 
-# -- Options for intersphinx extension ---------------------------------------
-
-# Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
 
 # -- Options for todo extension ----------------------------------------------
