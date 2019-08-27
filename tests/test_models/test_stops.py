dad15455f898c57eb06f7e9a7d53d995a0ee0cfe
Sergio Garcia Prado <sergio@garciparedes.me>
2019-08-27 17:42:47 +0200

    * Increased coverage of "Stop" unit tests. #35

--- tests/test_models/test_stops.py
+++ tests/test_models/test_stops.py
@@ -20,6 +20,10 @@ class TestStop(unittest.TestCase):
         self.assertEqual(stop.route, route)
         self.assertEqual(stop.position, position)
         self.assertEqual(stop.previous, None)
+        self.assertEqual(stop.previous_position, vehicle.initial)
+        self.assertEqual(stop.previous_departure_time, vehicle.earliest)
+        self.assertEqual(stop.navigation_time, stop.position.time_to(vehicle.initial, stop.previous_departure_time))
+        self.assertEqual(stop.waiting_time, 0.0)
 
     def test_creation_with_previous(self):
         vehicle = generate_one_vehicle()
@@ -38,6 +42,12 @@ class TestStop(unittest.TestCase):
         self.assertEqual(stop.position, position)
         self.assertEqual(stop.previous, previous_stop)
         self.assertEqual(stop.distance, stop.position.distance_to(previous_stop.position))
+        self.assertEqual(stop.previous_position, previous_stop.position)
+        self.assertEqual(stop.previous_departure_time, previous_stop.departure_time)
+        self.assertEqual(
+            stop.navigation_time,
+            stop.position.time_to(previous_stop.position, stop.previous_departure_time),
+        )
 
 
 if __name__ == '__main__':
