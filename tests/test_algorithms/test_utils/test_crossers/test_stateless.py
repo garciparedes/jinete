933d0e5dd890ce24420115d37555b408a5533aaa
Sergio Garcia Prado <sergio@garciparedes.me>
2019-10-26 22:18:09 +0200

    * Improved "TestStatelessCrosser" (2). #58

--- tests/test_algorithms/test_utils/test_crossers/test_stateless.py
+++ tests/test_algorithms/test_utils/test_crossers/test_stateless.py
@@ -21,6 +21,19 @@ class TestStatelessCrosser(unittest.TestCase):
         self.assertEqual(fleet, dispatcher.fleet)
         self.assertEqual(len(job.trips), len(list(dispatcher.iterator)))
 
+    def test_flush(self):
+        job = jit.Job(generate_trips(10), objective_cls=jit.DialARideObjective)
+        fleet = jit.Fleet(generate_vehicles(10))
+        dispatcher = jit.StatelessCrosser(
+            job=job,
+            fleet=fleet,
+        )
+        self.assertNotIn('iterator', dispatcher.__dict__)
+        list(dispatcher.iterator)
+        self.assertIn('iterator', dispatcher.__dict__)
+        dispatcher.flush()
+        self.assertNotIn('iterator', dispatcher.__dict__)
+
 
 if __name__ == '__main__':
     unittest.main()
