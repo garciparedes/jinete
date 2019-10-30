3af020615dae600c02f4e959bc507868030aab04
Sergio Garcia Prado <sergio@garciparedes.me>
2019-10-30 21:50:30 +0100

    * Included test for "Route.un_finish(...)" method. #60

--- tests/test_models/test_routes.py
+++ tests/test_models/test_routes.py
@@ -6,6 +6,7 @@ import jinete as jit
 from tests.utils import (
     generate_one_vehicle,
     generate_one_route,
+    generate_one_planned_trip,
 )
 
 
@@ -25,6 +26,32 @@ class TestRoutes(unittest.TestCase):
     def test_not_feasible_route(self):
         route = generate_one_route(False)
         self.assertFalse(route.feasible)
+
+    def test_un_finish(self):
+        route = generate_one_route()
+        vehicle = route.vehicle
+
+        self.assertEqual(vehicle.destination_position, route.last_position)
+        route.un_finish()
+        self.assertNotEqual(vehicle.destination_position, route.last_position)
+
+        stops_count = len(route.stops)
+        route.un_finish()
+        self.assertEqual(stops_count, len(route.stops))
+
+        planned_trip = generate_one_planned_trip(
+            feasible=True,
+            route=route,
+            earliest=route.last_departure_time,
+            origin=route.last_position,
+            destination=vehicle.destination_position,
+        )
+        route.append_planned_trip(planned_trip)
+        self.assertEqual(vehicle.destination_position, route.last_position)
+
+        stops_count = len(route.stops)
+        route.un_finish()
+        self.assertEqual(stops_count, len(route.stops))
 
 
 if __name__ == '__main__':
