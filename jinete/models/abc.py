14ad002c0daf733d15fe741e299d39907a455681
Sergio Garcia Prado <sergio@garciparedes.me>
2020-03-15 19:51:02 +0100

    Issue #101 * Working on docstring improvements on "jinete.models.abc.Model".

--- jinete/models/abc.py
+++ jinete/models/abc.py
@@ -1,3 +1,7 @@
+"""
+Abstract base classes used as base for the entities implemented on :mod:`~jinete.models` module.
+"""
+
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
@@ -12,6 +16,31 @@ if TYPE_CHECKING:
 
 
 class Model(ABC):
+    """
+    The abstract model class. The function of this class is to provide common methods such us casting or printing
+    functionality used by all the implemented models of the library.
+
+    .. seealso:: classes
+        :py:class:`~jinete.models.jobs.Job`,
+        :py:class:`~jinete.models.planned_trips.PlannedTrip`,
+        :py:class:`~jinete.models.plannings.Planning`,
+        :py:class:`~jinete.models.positions.Position`,
+        :py:class:`~jinete.models.positions.GeometricPosition`,
+        :py:class:`~jinete.models.results.Result`,
+        :py:class:`~jinete.models.routes.Route`,
+        :py:class:`~jinete.models.services.Service`,
+        :py:class:`~jinete.models.stops.Stop`,
+        :py:class:`~jinete.models.surfaces.Surface`,
+        :py:class:`~jinete.models.trips.Trip`,
+        :py:class:`~jinete.models.vehicles.Fleet`,
+        :py:class:`~jinete.models.vehicles.Vehicle`,
+    """
+
+    def __init__(self):
+        """
+        The constructor of the class.
+        """
+
     def __repr__(self):
         return self._print(dict(self))
 
