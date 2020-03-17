799b8e83637606ada104d2f8e4592b0a3b010dd0
Sergio Garcia Prado <sergio@garciparedes.me>
2020-03-17 22:34:46 +0100

    Issue #101 * Added "jinete.exceptions" initial documentation.

--- jinete/utils/collections.py
+++ jinete/utils/collections.py
@@ -1,9 +1,27 @@
-from typing import List, TypeVar, Set
+"""
+A set of utilities related with collections.
+"""
+from __future__ import annotations
 
-E = TypeVar('E')
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from typing import (
+        List,
+        TypeVar,
+        Set,
+    )
+
+    E = TypeVar('E')
 
 
 def remove_duplicates(seq: List[E]) -> List[E]:
+    """
+    Removes the ducplicated elements of a list, preserving its original order.
+
+    :param seq: The original list.
+    :return: A list with elements ordered as a in the initial one, but removing the duplicated elements.
+    """
     seen: Set[E] = set()
     seen_add = seen.add
     return [x for x in seq if not (x in seen or seen_add(x))]
