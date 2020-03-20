b1519c6f2cec51f1cfc6be3a65cfbcc4c759595e
Sergio Garcia Prado <sergio@garciparedes.me>
2020-03-20 21:07:21 +0100

    Issue #101 * Improved "jinete.exceptions" documentation.

--- jinete/exceptions.py
+++ jinete/exceptions.py
@@ -1,6 +1,4 @@
-"""
-Defines the hierarchy of exceptions raised and used by `jinete`'s module.
-"""
+"""Defines the hierarchy of exceptions raised and used by `jinete`'s module."""
 
 from __future__ import annotations
 
@@ -18,32 +16,26 @@ if TYPE_CHECKING:
 
 
 class JineteException(Exception):
-    """
-    The base exception on `jinete`'s package.
-    """
+    """The base exception on `jinete`'s package."""
 
     def __init__(self, message: str):
-        """
-        The constructor of the class.
+        """Construct a new instance.
 
         :param message: A brief description of the cause.
         """
-
         super().__init__(message)
         self.message = message
 
 
 class PreviousStopNotInRouteException(JineteException):
-    """
-    Represents an exception raised while the previous stop is not present in the target route.
-    """
+    """Represents an exception raised while the previous stop is not present in the target route."""
 
     def __init__(self, route: Route, stop: Stop):
-        """
+        """Construct a new instance.
+
         :param route: The route without the previous stop.
         :param stop: The stop that contains the non present previous stop.
         """
-
         self.route = route
         self.stop = stop
         message = f'Stop "{stop}" has no previous "{stop.previous}" on the belonging route "{route}".'
@@ -51,15 +43,13 @@ class PreviousStopNotInRouteException(JineteException):
 
 
 class NonFeasiblePlannedTripException(JineteException):
-    """
-    Represents an exception raised while providing a non feasible route.
-    """
+    """Represents an exception raised while providing a non feasible route."""
 
     def __init__(self, planned_trip: PlannedTrip):
-        """
+        """Construct a new instance.
+
         :param planned_trip: The non feasible planned trip.
         """
-
         assert planned_trip.feasible is False
 
         self.planned_trip = planned_trip
@@ -68,15 +58,13 @@ class NonFeasiblePlannedTripException(JineteException):
 
 
 class NonFeasibleRouteException(JineteException):
-    """
-    Represents an exception raised while providing a non feasible route.
-    """
+    """Represents an exception raised while providing a non feasible route."""
 
     def __init__(self, route: Route):
-        """
+        """Construct a new instance.
+
         :param route: The non feasible route.
         """
-
         assert route.feasible is False
 
         self.route = route
